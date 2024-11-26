c596f04d8ea0eef4130c2b48037a5ce5cc23f7f2
Gerasimos Tzakis <gertzakis@gmail.com>
2024-11-26 09:44:57 +0200

    chore: add ruff linter in invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,25 +1,12 @@
 """Tasks for use with Invoke."""
 
-import os
-import sys
-from invoke import task
-
-try:
-    import toml
-except ImportError:
-    sys.exit(
-        "Please make sure to `pip install toml` or enable the Poetry shell and run `poetry install`."
-    )
+from invoke.exceptions import Exit
+from invoke.tasks import task
 
 # TODO: fix tasks.py to use poetry for running tests
-# add task for ruff and better usage in general
-PYPROJECT_CONFIG = toml.load("pyproject.toml")
-TOOL_CONFIG = PYPROJECT_CONFIG["tool"]["poetry"]
-# Can be set to a separate Python version to be used for launching or building image
-PYTHON_VER = os.getenv("PYTHON_VER", "3.11")
 
 
-def run_cmd(context, exec_cmd):
+def run_command(context, command, **kwargs):
     """Wrapper to run the invoke task commands.
 
     Args:
@@ -30,43 +17,107 @@ def run_cmd(context, exec_cmd):
     Returns:
         result (obj): Contains Invoke result from running task.
     """
-    result = context.run(exec_cmd, pty=True)
+    result = context.run(command, **kwargs)
     return result
 
 
-@task()
+# ------------------------------------------------------------------------------
+# BUILD
+# ------------------------------------------------------------------------------
+@task(
+    help={
+        "check": (
+            "If enabled, check for outdated dependencies in the poetry.lock file, "
+            "instead of generating a new one. (default: disabled)"
+        )
+    }
+)
+def lock(context, check=False):
+    """Generate poetry.lock."""
+    run_command(context, f"poetry {'check' if check else 'lock --no-update'}")
+
+
+# ------------------------------------------------------------------
+# TESTS
+# ------------------------------------------------------------------------------
+
+
+@task
 def black(context):
     """Run black to check that Python files adherence to black standards."""
     exec_cmd = "black --check --diff ."
-    run_cmd(context, exec_cmd)
+    run_command(context, exec_cmd)
 
 
-@task()
+@task
 def flake8(context):
     """Run flake8 code analysis."""
     exec_cmd = "flake8 . --config .flake8"
-    run_cmd(context, exec_cmd)
+    run_command(context, exec_cmd)
 
 
-@task()
+@task
 def pylint(context):
     """Run pylint code analysis."""
     exec_cmd = 'find . -name "*.py" | xargs pylint'
-    run_cmd(context, exec_cmd)
+    run_command(context, exec_cmd)
 
 
-@task()
+@task
 def yamllint(context):
-    """Run yamllint to validate formatting adheres to NTC defined YAML standards."""
-    exec_cmd = "yamllint ."
-    run_cmd(context, exec_cmd)
+    """Run yamllint to validate formatting adheres to NTC defined YAML standards.
+
+    Args:
+    ----
+        context (obj): Used to run specific commands
+    """
+    command = "yamllint . --format standard"
+    run_command(context, command)
 
 
-@task()
+@task
 def bandit(context):
     """Run bandit to validate basic static code security analysis."""
     exec_cmd = "bandit --recursive ./ --configfile .bandit.yml"
-    run_cmd(context, exec_cmd)
+    run_command(context, exec_cmd)
+
+
+@task(
+    help={
+        "action": "Available values are `['lint', 'format']`. Can be used multiple times. (default: `['lint', 'format']`)",
+        "target": "File or directory to inspect, repeatable (default: all files in the project will be inspected)",
+        "fix": "Automatically fix selected actions. May not be able to fix all issues found. (default: False)",
+        "output_format": "See https://docs.astral.sh/ruff/settings/#output-format for details. (default: `concise`)",
+    },
+    iterable=["action", "target"],
+)
+def ruff(context, action=None, target=None, fix=False, output_format="concise"):
+    """Run ruff to perform code formatting and/or linting."""
+    if not action:
+        action = ["lint", "format"]
+    if not target:
+        target = ["."]
+
+    exit_code = 0
+
+    if "format" in action:
+        command = "ruff format "
+        if not fix:
+            command += "--check "
+        command += " ".join(target)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
+
+    if "lint" in action:
+        command = "ruff check "
+        if fix:
+            command += "--fix "
+        command += f"--output-format {output_format} "
+        command += " ".join(target)
+        if not run_command(context, command, warn=True):
+            exit_code = 1
+
+    raise Exit(code=exit_code)
 
 
 @task()
@@ -77,6 +128,7 @@ def tests(context):
     pylint(context)
     yamllint(context)
     bandit(context)
+    ruff(context)
     # pytest(context)
 
     print("All tests have passed!")
