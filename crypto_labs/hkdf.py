a634bbd71d7b275943d8c08f3c723c46f1b330a2
Gerasimos Tzakis <gertzakis@gmail.com>
2024-12-07 15:00:53 +0200

    add first draft of hkdf_expand

--- crypto_labs/hkdf.py
+++ crypto_labs/hkdf.py
@@ -25,6 +25,44 @@ def hkdf_extract(salt: bytes, input_key_material: bytes, hash_function: str) -> 
     return hmac.new(salt, input_key_material, hash_function).digest()
 
 
+def hkdf_expand(pseudo_random_key: bytes, info: bytes, key_length: int, hash_function: str) -> bytes:
+    """HKDF Expand function that uses HMAC as the pseudorandom function.
+
+    Args:
+        pseudo_random_key (bytes): The pseudo-random key to use for the HMAC function.
+        info (bytes): The context and application specific information.
+        key_length (int): The length of the output keying material in bytes.
+        hash_function (str): The hash function to use.
+
+    Returns:
+        bytes: The expanded key.
+    """
+    hash_length = hmac.new(b"", b"", hash_function).digest_size
+    if key_length > 255 * hash_length:
+        raise ValueError("Key length too long. Cannot expand output keying material bigger than `255 * Hash's length`.")
+
+    # The number of blocks to use for the HMAC function
+    num_blocks = key_length // hash_length + (1 if key_length % hash_length else 0)
+    block_result = b""
+    # The expanded key
+    expanded_key = b""
+    block_number = 1
+
+    # while len(expanded_key) < key_length:
+    #     block_result = hmac.new(pseudo_random_key, block_result + info + bytes([block_number]), hash_function).digest()
+    #     expanded_key += block_result
+    #     block_number += 1
+
+
+    # The output of the HMAC function is the expanded key
+    for i in range(num_blocks):
+        print(i)
+        block_result = hmac.new(pseudo_random_key, block_result + info + bytes([i + 1]), hash_function).digest()
+        expanded_key += block_result
+
+    return expanded_key[:key_length]
+
+
 # TODO add HKDF implementation
 def hkdf():
     """Have to implement HKDF funciton."""
--- crypto_labs/tests/test_hkdf.py
+++ crypto_labs/tests/test_hkdf.py
@@ -3,23 +3,29 @@
 import unittest
 
 # from hashlib import pbkdf2_hmac
-
-# from crypto_labs.hkdf import hkdf
-
+from crypto_labs.hkdf import hkdf_extract, hkdf_expand
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
 
 class TestHKDF(unittest.TestCase):
     """Test case for HKDF module."""
 
 
-    def test_hkdf_extract(self):
+    def test_hkdf_expand(self):
         """Test that hkdf_extract function returns the expected key."""
-        # TODO: test return key length is correct
+        prk = b"pseudorandomkey"
+        info=b"makis"
+
+        key = hkdf_expand(prk, info, 5, "sha256")
+        self.assertEqual(key, HKDF(hashes.SHA256(), 5, prk, info).derive(info))
+
+
 
 
     # TODO: add test cases for HKDF
     def test_hkdf(self):
         """Test that hkdf function returns the expected key."""
-        # TODO: implement test cases for hkdf function
+
 
 
 if __name__ == "__main__":
